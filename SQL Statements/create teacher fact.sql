5cda973fb2bbb989a849a8c2232a2e79859056de
HsiaoSt <hsiao.stephen5@gmail.com>
2017-11-19 15:21:45 +0800

    Removed typekey
    removed typekey from teacherf and create type dimension file

--- SQL Statements/create teacher fact.sql
+++ SQL Statements/create teacher fact.sql
@@ -4,26 +4,20 @@ drop table if exists teacherf;
 
 create table teacherf
     (
-    countrykey varchar(3) not null, compensationkey int not null, typekey varchar(3) not null, yearkey varchar(6) not null,
+    countrykey varchar(3) not null, compensationkey int not null, yearkey varchar(6) not null,
     femalepercentage double, malepercentage double, 
     primaryfpercentage double, secondaryfpercentage double, tertiaryfpercentage double,
     primarympercentage double, secondarympercentage double, tertiarympercentage double,
     
-        primary key (countrykey, compensationkey, typekey, yearkey),
+        primary key (countrykey, compensationkey, yearkey),
         
         foreign key(countrykey) references countryd(countrykey),
         foreign key(compensationkey) references compensationd(compensationkey),
-        foreign key(typekey) references typed(typekey),
         foreign key(yearkey) references yeard(yearkey)
 	) 
 select 
 	countrycode as countrykey,
     compensationkey as compensationkey,
-    case substring_index(seriesname, " education", 1)
-		when 'primary' then 'P.F'
-        when 'secondary' then 'S.F'
-        when 'tertiary' then 'T.F'
-	end as typekey,
 	substring(YearC, 7, 6) as yearkey
 from 
 	(((advandb_mco2_wdi.databyyear natural join advandb_mco2_wdi.country)
@@ -33,4 +27,4 @@ from
 where
 	seriescode LIKE 'SE.%.TCHR.FE.ZS'
 group by
-	countrykey, typekey;
+	countrykey, yearkey;
